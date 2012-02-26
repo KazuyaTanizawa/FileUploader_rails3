74b7993713c2762d2d6d718137599630c267d0e3
kazuya tanizawa <kazuyat7175@gmail.com>
2012-02-27 08:23:19 +0900

    first commit

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,106 @@
+class PhotosController < ApplicationController
+  # GET /photos
+  # GET /photos.json
+  def index
+    @photos = Photo.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @photos }
+    end
+  end
+
+  # GET /photos/1
+  # GET /photos/1.json
+  def show
+    @photo = Photo.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @photo }
+    end
+  end
+
+  # GET /photos/new
+  # GET /photos/new.json
+  def new
+    @photo = Photo.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @photo }
+    end
+  end
+
+  # GET /photos/1/edit
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  # POST /photos
+  # POST /photos.json
+  def create
+    @photo = Photo.new(params[:photo])
+
+    respond_to do |format|
+      if @photo.save
+        format.html { redirect_to @photo, notice: 'Photo was successfully created.' }
+        format.json { render json: @photo, status: :created, location: @photo }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @photo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /photos/1
+  # PUT /photos/1.json
+  def update
+    @photo = Photo.find(params[:id])
+
+    respond_to do |format|
+      if @photo.update_attributes(params[:photo])
+        format.html { redirect_to @photo, notice: 'Photo was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @photo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /photos/1
+  # DELETE /photos/1.json
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+
+    respond_to do |format|
+      format.html { redirect_to photos_url }
+      format.json { head :no_content }
+    end
+  end
+  def up    
+    file=params[:qqfile]
+    if request.xhr?
+      if request.body.length == request.headers['CONTENT_LENGTH'].to_i
+        file_data = request.body.read 
+        file_name = file
+      end
+    else
+      if file.instance_of?(File)
+        file_data = file.read 
+        file_name = file.original_filename
+      end
+    end    
+   @photo = Photo.new
+   @photo.filename = file_name
+   File.open("./public/images" + file_name, 'wb') do |of|
+    of.write(file_data)
+   @photo.save
+    render :json => { :success => true }
+   end
+  end
+  def upload
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fileuploader::Application.config.secret_token = 'fe99fa22704b8846959e6fcaa72cf0a35af03c5184b7ab0943a8fe528b3006a7542062dad4343e69f2d680b4bba47aa666a28de373f285ba82413091afdc1158'
